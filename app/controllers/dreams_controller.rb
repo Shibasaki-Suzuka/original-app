a1215e49849b40cc12b4ec77073b258c977dc928
Shibasaki-Suzuka <nekonosiawase25@gmail.com>
2023-04-21 21:52:21 +0900

    ログイン・ログアウトの表示切替

--- app/controllers/dreams_controller.rb
+++ app/controllers/dreams_controller.rb
@@ -1,4 +1,13 @@
 class DreamsController < ApplicationController
+  before_action :move_to_index, except: :index
+
   def index
+  end
+
+  private
+  def move_to_index
+    unless user_signed_in?
+      redirect_to new_user_registration_path
+    end
   end
 end
