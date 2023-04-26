8cffeda779e502530a6c76a514f916e618e88e26
Shibasaki-Suzuka <nekonosiawase25@gmail.com>
2023-04-26 17:41:08 +0900

    view修正

--- app/models/dream.rb
+++ app/models/dream.rb
@@ -1,5 +1,5 @@
 class Dream < ApplicationRecord
-  validates :dream_list, null: false
+  validates :dream_list, presence: true
   validates :cost, format: { with: /\A[0-9]+\z/, message: 'は半角数値を使用してください' }, allow_blank: true
   
   belongs_to :user
