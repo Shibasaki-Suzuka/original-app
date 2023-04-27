3af1e424ac377f27ddae79f1e374ebcf686b0e1f
Shibasaki-Suzuka <nekonosiawase25@gmail.com>
2023-04-27 22:56:57 +0900

    ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "dreams#index"
-  resources :dreams, only: [:index, :new, :create, :show, :edit, :update]
-  
+  resources :dreams, only: [:index, :new, :create, :show, :edit, :update] do
+    resources :achieve, only: :create
+  end
   resources :users, only: :show
 end
