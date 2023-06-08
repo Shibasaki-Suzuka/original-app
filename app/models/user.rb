846ad4bcb10759fd4d249f324e824486da4a3cb9
Shibasaki-Suzuka <nekonosiawase25@gmail.com>
2023-06-08 18:04:20 +0900

    ストロングパラメータ設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,6 +15,7 @@ class User < ApplicationRecord
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
   validates :password, format: { with: VALID_PASSWORD_REGEX, message: 'は半角英数を両方含む必要があります' }
 
-
   has_many :dreams
+
+  mount_uploader :avatar, AvatarUploader
 end
