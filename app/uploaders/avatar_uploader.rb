73ce4a41171810e8154a6d3f7406bb41510ec17d
Shibasaki-Suzuka <nekonosiawase25@gmail.com>
2023-06-15 22:44:30 +0900

    画像未設定時の設定

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -2,7 +2,9 @@ class AvatarUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
-
+  version :thumb do
+    process resize_to_fit: [100, 100]
+  end
   # Choose what kind of storage to use for this uploader:
   storage :file
   # storage :fog
@@ -12,7 +14,9 @@ class AvatarUploader < CarrierWave::Uploader::Base
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
-
+  def default_url
+    "noavatar.png"
+  end
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url(*args)
   #   # For Rails 3.1+ asset pipeline compatibility:
