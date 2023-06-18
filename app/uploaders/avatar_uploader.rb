bbf6d63eb6bd328bb981181e56c29847b86998aa
Shibasaki-Suzuka <nekonosiawase25@gmail.com>
2023-06-18 23:26:10 +0900

    エラー修正

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -1,10 +1,10 @@
 class AvatarUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
-  version :thumb do
-    process resize_to_fit: [100, 100]
-  end
+  include CarrierWave::MiniMagick
+  # version :thumb do
+  process resize_to_fit: [100, 100]
+  # end
   # Choose what kind of storage to use for this uploader:
   storage :file
   # storage :fog
