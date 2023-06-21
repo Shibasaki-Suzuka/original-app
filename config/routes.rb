420e9fed132839a964f17903357e3126afd14991
Shibasaki-Suzuka <nekonosiawase25@gmail.com>
2023-06-21 18:17:29 +0900

    ユーザー情報の編集・保存設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'registrations' }
   root to: "dreams#index"
   resources :dreams, only: [:index, :new, :create, :show, :edit, :update] do
     resources :achieves, only: [:index, :create]
