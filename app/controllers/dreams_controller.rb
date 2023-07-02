b95f24514638eae3cedc9c083b3d3f4ee1fb433b
Shibasaki-Suzuka <nekonosiawase25@gmail.com>
2023-07-02 20:49:06 +0900

    夢のカウント機能

--- app/controllers/dreams_controller.rb
+++ app/controllers/dreams_controller.rb
@@ -2,7 +2,10 @@ class DreamsController < ApplicationController
   before_action :move_to_index, except: :index
 
   def index
-    @dreams = Dream.includes(:user)
+    query = "SELECT id,dream_list,cost,due,memo,user_id FROM dreams WHERE user_id = ?"
+    @dreams = Dream.find_by_sql([query, current_user])
+    count_query = "SELECT COUNT(*) FROM dreams WHERE user_id = ? GROUP BY user_id"
+    @dreams_count = Dream.count_by_sql([count_query, current_user])
   end
 
   def new
