8b27130664a7d5612010745c83acafc6dbbd1a27
Shibasaki-Suzuka <nekonosiawase25@gmail.com>
2023-07-03 22:45:31 +0900

    修正

--- db/migrate/20230427134019_create_achieves.rb
+++ db/migrate/20230427134019_create_achieves.rb
@@ -1,10 +1,10 @@
 class CreateAchieves < ActiveRecord::Migration[6.0]
   def change
     create_table :achieves do |t|
-      t.integer    :user_id
       t.text       :success_day, null: false
       t.text       :comment
       t.references :dream,       null: false, foreign_key: true
+      t.references :user,       null: false, foreign_key: true
 
       t.timestamps
     end
