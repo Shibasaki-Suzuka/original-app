7bb0ebd55c7060fc830eeff3eef6e057ef487d45
Shibasaki-Suzuka <nekonosiawase25@gmail.com>
2023-08-01 23:00:05 +0900

    非同期化

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,13 +1,19 @@
 class LikesController < ApplicationController
+  before_action :set_dream
+
   def create
     like = current_user.likes.build(dream_id: params[:dream_id])
     like.save
-    redirect_to root_path
+    render 'create.js.erb'
   end
 
   def destroy
     like = Like.find_by(dream_id: params[:dream_id], user_id: current_user.id)
     like.destroy
-    redirect_to root_path
+    render 'destroy.js.erb'
+  end
+
+  def set_dream
+    @dream = Dream.find(params[:dream_id])
   end
 end
