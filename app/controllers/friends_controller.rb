9cd5b0b0b2efcd0226b3275cd3c4030796da7669
Shibasaki-Suzuka <nekonosiawase25@gmail.com>
2023-08-11 18:46:33 +0900

    非同期処理

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -1,13 +1,19 @@
 class FriendsController < ApplicationController
+  before_action :set_user
+
   def create
     follow = current_user.active_friends.new(follower_id: params[:user_id])
     follow.save
-    redirect_to root_path
+    render 'create.js.erb'
   end
 
   def destroy
     follow = current_user.active_friends.find_by(follower_id: params[:user_id])
     follow.destroy
-    redirect_to root_path
+    render 'destroy.js.erb'
+  end
+
+  def set_user
+    @user = User.find(params[:user_id])
   end
 end
